9b08528b2b5293e2e61cc513a25801fd6e2c0b37
Juan Chen <juanchen917@gmail.com>
2018-05-20 21:39:07 -0700

    first commit

---
+++ api.py
@@ -0,0 +1,194 @@
+
+import requests
+import util
+from flask_login import current_user
+import inspect
+
+TEST_LOG = __name__ + '.py'
+
+ERROR_LOG = __name__ + '.py ' + 'ERROR: '
+
+SPRING_URL = 'http://192.168.1.66:8080/'
+
+cur_survey_id = None
+
+
+def test_log(func, text=''):
+    print('In {0} func: {1}, msg: {2}'.format(TEST_LOG, func, text))
+
+
+def error_log(func, err=''):
+    print('In {0} func: {1}, msg: {2}'.format(ERROR_LOG, func, err))
+
+
+def get_code(email):
+    try:
+        url = SPRING_URL+'email_verify?email='+email
+        test_log(inspect.stack()[0][3], url)
+        rev = requests.get(SPRING_URL+'email_verify?email='+email)
+        print(TEST_LOG + str(rev.status_code))
+        if rev.status_code == 200:
+            return True
+        print(ERROR_LOG + 'Get Code Error')
+        print(TEST_LOG + rev.text)
+        return False
+    except Exception as e:
+        print(ERROR_LOG + 'Exception: Get Code Error')
+        print(e)
+        return False
+
+
+def register_user(data):
+    try:
+        email = data['username']
+        code = data['code']
+        password = data['password']
+        url = SPRING_URL+'signup?email='+email+'&code='+code+'&password='+password
+        test_log(inspect.stack()[0][3], url)
+        rev = requests.post(url)
+        if rev.status_code == 200:
+            return True
+        error_log(inspect.stack()[0][3], 'register failed')
+        error_log(inspect.stack()[0][3], 'rev.text')
+        return False
+    except Exception as e:
+        print(e)
+        error_log(inspect.stack()[0][3], "Exception: register failed")
+        return False
+
+
+def get_user_id(username):
+    try:
+        rev = requests.get(SPRING_URL + 'api/get_user_id?email=' + username)
+        json = util.string_to_json(rev.text)
+        print(json)
+        return str(json['id'])
+    except Exception as e:
+        print (e)
+        error_log(inspect.stack()[0][3], "get user id failed")
+        return None
+
+
+def get_username(user_id):
+    try:
+        rev = requests.get(SPRING_URL + 'account/' + user_id)
+        json = util.string_to_json(rev.text)
+        if json:
+            return json['email']
+    except Exception as e:
+        print(e)
+        error_log(inspect.stack()[0][3], "get username error")
+        return None
+
+
+def get_password(username):
+    try:
+        rev = requests.get(SPRING_URL + 'api/get_user_id?email=' + username)
+        json = util.string_to_json(rev.text)
+        return json['password']
+    except Exception as e:
+        print(e)
+        error_log(inspect.stack()[0][3], "get password failed")
+        return None
+
+
+# Return a json from Spring
+def create_survey(data):
+    # @PostMapping(value="/account/{accountId}/addsurvey")
+    url = SPRING_URL + 'account/{0}/addsurvey'.format(current_user.get_id())
+    test_log(inspect.stack()[0][3], url)
+    rev = requests.post(url, json=data)
+    json = util.string_to_json(rev.text)
+    return json
+
+
+def get_all_surveys_for_user(uid):
+    '''
+    :return a list of survey object string
+    '''
+    url = SPRING_URL + 'account/' + str(uid) + '/allsurveys'
+    test_log(inspect.stack()[0][3], url)
+    rev = requests.get(url)
+    json = util.string_to_json(rev.text)
+    return json
+
+
+def get_survey_by_id(surveyId):
+    url = SPRING_URL + 'survey?surveyId=' + str(surveyId)
+    test_log(inspect.stack()[0][3], url)
+    rev = requests.get(url)
+    json = util.string_to_json(rev.text)
+    cur_survey_id = json['id']
+    return json
+
+
+# uuid: link
+def get_survey_by_uuid(uuid):
+    url = SPRING_URL + 'survey/' + uuid
+    test_log(inspect.stack()[0][3], url)
+    rev = requests.get(url)
+    json = util.string_to_json(rev.text)
+    return json
+
+
+def create_question(data):
+    surveyId = data['surveyId']
+    url = SPRING_URL + 'addquestion/' + str(surveyId)
+    test_log(inspect.stack()[0][3], url)
+    data['surveyId'] = int(surveyId)
+    rev = requests.post(url, json=data)
+    if rev.status_code == 200:
+        print('Rev from POST: ' + url +rev.text)
+        return util.string_to_json(rev.text)
+    return False
+
+
+def get_all_general_surveys():
+    url = SPRING_URL + 'survey/surveyType/' + 'GENERAL'
+    test_log(inspect.stack()[0][3], url)
+    rev = requests.get(url)
+    json = util.string_to_json(rev.text)
+    return json
+
+
+def create_answer(answer):
+    survey_link = answer['surveyLink']
+    url = SPRING_URL + 'answer/' + survey_link
+    test_log(inspect.stack()[0][3], url)
+    rev = requests.post(url, json=answer)
+    if rev.status_code is 200:
+        print('Rev from POST : '+ url +rev.text)
+        return util.string_to_json(rev.text)
+    else:
+        error = "[SPRING return error]" + str(rev.status_code)
+        return util.string_to_json(rev.text)
+
+
+def send_invitations(json):
+    userId = json['userId']
+    surveyId = json['surveyId']
+    if userId is not None and surveyId is not None and json['emails'] is not None:
+        url = SPRING_URL + 'account/' + userId + "/survey/" + surveyId + "/invitation?emails=" + json['emails']
+        test_log(inspect.stack()[0][3], url)
+        rev = requests.post(url=url)
+        test_log(inspect.stack()[0][3], rev.text)
+        return rev.text
+    return "Para Error, not sent"
+
+
+# TODO: get report by survey id
+# return json string/object
+def get_report_by_id(uid, sid):
+    # GET http://localhost:8080/account/1/report/?surveyId=8
+    url = (SPRING_URL + 'account/{0}/report/?surveyId={1}'.format(uid, sid))
+    test_log(inspect.stack()[0][3], url)
+    rev = requests.get(url)
+    test_log(inspect.stack()[0][3], rev.text)
+    json = util.string_to_json(rev.text)
+    return json
+
+
+if __name__ == '__main__':
+    get_user_id("juanchen917@gmail.com")
+    get_username("2")
+    get_password("juanchen917@gmail.com")
---
+++ util.py
@@ -0,0 +1,144 @@
+import json as JSON
+from flask import json
+
+request_json = None
+
+ERROR_LOG = __name__ + ' juanchen: '
+
+
+def request_form_to_json(request):
+    try:
+        return JSON.loads(json.dumps(request.form))
+    except Exception:
+        print(ERROR_LOG + 'request form to json failed')
+        return None
+
+
+def string_to_json(json_str):
+    try:
+        return JSON.loads(json_str)
+    except Exception:
+        print(ERROR_LOG + 'string to json failed')
+        return None
+
+
+def json_to_string(json_obj):
+    try:
+        return JSON.dumps(json_obj)
+    except Exception:
+        print(ERROR_LOG + 'json to string failed')
+        return None
+
+
+def stringlist_to_json_list(data):
+    try:
+        options = []
+        for choice in data.split(';'):
+            options.append({'content': choice})
+        return options
+    except Exception:
+        print(ERROR_LOG + 'string list to json list failed')
+        return None
+
+
+def extract_survey_json(survey):
+    # input: json format survey
+    try:
+        if survey['questions']:
+            for question in survey['questions']:
+                if question['questionContent']:
+                    content = question['questionContent']['questionContent']
+                    question['questionContent'] = stringlist_to_json_list(content)
+        print(survey)
+        return survey
+    except Exception:
+        return None
+
+
+def statis_to_json_list(statis, questionContent):
+    try:
+        options = []
+        stat = []
+        res = []
+        for count in statis.split(';'):
+            stat.append(count)
+        print(stat)
+        for choice in questionContent.split(';'):
+            options.append(choice)
+        print(options)
+        for i in range(len(options)):
+            res.append({'content': options[i], 'count': stat[i]})
+        print(res)
+        return res
+    except Exception:
+        print(ERROR_LOG + 'string list to json list failed')
+        return None
+
+
+def extract_report_json(survey):
+    # input: json format survey
+    try:
+        if survey['questions']:
+            for question in survey['questions']:
+                statis = question['statistic']
+                if question['questionContent']:
+                    content = question['questionContent']['questionContent']
+                    question['questionContent'] = statis_to_json_list(statis, content)
+                else:
+                    res = []
+                    for val in statis.split(';'):
+                        if val.strip() != '':
+                            res.append({'content': val})
+                    question['questionContent'] = res
+            print(survey)
+            return survey
+    except Exception as e:
+        print(e)
+        return None
+
+# {'10.answerContent': 'yes', '20.answerContent': 'cookie', '21.answerContent.Japan': 'Japan',
+# '21.answerContent.Korea': 'Korea', '22.answerContent': 'female', '23.answerContent.engineer': 'engineer',
+# '23.answerContent.freelancer': 'freelancer', '8.answerContent': 'Anything you like.', 'action': 'save_answer',
+# 'username': 'juanchen917@gmail.com'}
+def parse_answer_json(json):
+    res = {}
+    for k, v in json.items():
+        if len(k.split('.')) > 1:
+            tmp_key =k.split('.')[0]
+            if tmp_key not in res:
+                res[tmp_key] = []
+            res[tmp_key].append(v)
+        else:
+            res[k] = v
+# {'10': ['yes'], '20': ['cookie'], '21': ['China', 'Japan'], '22': ['female'], '23': ['engineer'],
+#              '8': ['nohting to say'], 'action': 'save_answer', 'username': 'juanchen917@gmail.com'}
+
+    print(res)
+    answer = {}
+    questions = []
+    for k,v in res.items():
+        if type(v) is list:
+            question = {}
+            question['questionId'] = int(k)
+            question['answerContent'] = ';'.join(v)
+            questions.append(question)
+        else:
+            if k == 'username':
+                answer['email'] = v
+            else:
+                answer[k] = v
+    answer['aq'] = questions
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
